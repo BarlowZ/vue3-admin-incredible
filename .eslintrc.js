7af58e3cae2e9ba7a978427b395baf8682a597fd
zhuangguangqian <zhuangguangqian@szkingdom.com>
2022-05-11 00:59:22 +0800

    feat: 测试

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 1.5
+ * @Author: qian
+ * @Date: 2022-05-11 00:28:20
+ * @LastEditors: qian
+ * @LastEditTime: 2022-05-11 00:44:20
+ */
 module.exports = {
   root: true,
   env: {
@@ -12,6 +20,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'indent': 0,
+    'space-before-function-paren': 0
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
+<!--
+ * @Descripttion:
+ * @version: 1.5
+ * @Author: qian
+ * @Date: 2022-05-11 00:28:20
+ * @LastEditors: qian
+ * @LastEditTime: 2022-05-11 00:44:37
+-->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
