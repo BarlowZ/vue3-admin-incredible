bd457b84323dfa846ec4726769f2e3ca79424e97
BarlowZ <739005443@qq.com>
2022-05-15 01:27:48 +0800

    feat: 添加用户管理列表以及搜索功能

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import 'element-plus/dist/index.css'
 import '@/router/permission'
 import * as ELIcons from '@element-plus/icons-vue'
 import i18n from '@/i18n'
+import filters from './utils/filters'
 
 // 实例化pinia
 const pinia = createPinia()
@@ -17,6 +18,7 @@ const app = createApp(App)
 for (const iconName in ELIcons) {
   app.component(iconName, ELIcons[iconName])
 }
+filters(app)
 
 SvgIcon(app)
 app.use(pinia).use(router).use(i18n).mount('#app')
